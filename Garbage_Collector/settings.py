49ec5ce5c369f8507fd952dee87a4693c2fd5801
Ranjithkumar <ranjithkumar-radhakrishnan@ideas2it.com>
2024-09-24 14:08:28 +0530

    Added the migrations, pycache directory into .gitignore file

--- Garbage_Collector/settings.py
+++ Garbage_Collector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'customer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Garbage_Collector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'garbage_collector',
+        'USER': 'postgres',
+        'PASSWORD': 'Ranjith@492',
+        'PORT': '5432',
+        'Host': 'localhost'
     }
 }
 
--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,16 @@
+import uuid
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+
+
+class User(models.Model):
+    userId = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    email = models.EmailField(max_length=320, null=False)
+    phone_number = models.CharField(max_length=10, null=False)
+    password = models.CharField(max_length=100, null=False)
+
+
